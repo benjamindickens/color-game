67a33b642d46134ae2afa0d295cff577ea034a3f
benjamindickens <59678530+benjamindickens@users.noreply.github.com>
2020-04-09 18:46:51 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -3,35 +3,72 @@ $(".lol").slideUp(100);
 // alert(
 //   "Правила: Запомни последовательность цветов и повтори их. Каждый уровень показывается  только новый цвет. Повтори предыдущие и добавь новый. Удачи! :P ( P.S первые пару конов лагает звук >< )"
 // );
+
+let audio = {
+  wrong: new Audio("./sounds/wrong.mp3"),
+  yellow: new Audio(`./sounds/yellow.mp3`),
+  red: new Audio(`./sounds/red.mp3`),
+  blue: new Audio(`./sounds/blue.mp3`),
+  green: new Audio(`./sounds/green.mp3`),
+  white: new Audio(`./sounds/white.mp3`),
+  purple: new Audio(`./sounds/purple.mp3`),
+};
 let userClickedPattern = [];
-let buttonColors = ["red", "blue", "green", "yellow"];
+let buttonColors = ["red", "blue", "green", "yellow", "white", "purple"];
 let gamePattern = [];
 let lvl = 0;
-function nextSequence() {
-  let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-  //animation
-  lvl++;
-  $("h1").html(`Level<br> ${lvl}`);
 
-  let i = 1;
-  if (gamePattern.length === 1) {
-    setTimeout(() => {
+function nextSequence() {
+  if (lvl < 10) {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    lvl++;
+    $("h1").html(`Level<br> ${lvl}`);
+    
+    let i = 1;
+    if (gamePattern.length === 1) {
+      setTimeout(() => {
+        $(`#${gamePattern[0]}`).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(gamePattern[0]);
+      }, 500);
+    } else {
       $(`#${gamePattern[0]}`).fadeIn(100).fadeOut(100).fadeIn(100);
       playSound(gamePattern[0]);
+    }
+    setInterval(function () {
+      $(`#${gamePattern[i]}`).fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound(gamePattern[i]);
+      i++;
+      if (i === gamePattern.length) clearInterval(interval);
     }, 500);
   } else {
-    $(`#${gamePattern[0]}`).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(gamePattern[0]);
+    $("#white").removeClass("lvl1");
+    $("#purple").removeClass("lvl1");
+    let randomNumber = Math.floor(Math.random() * 6);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    //animation
+    lvl++;
+    $("h1").html(`Level<br> ${lvl}`);
+    let i = 1;
+    if (gamePattern.length === 1) {
+      setTimeout(() => {
+        $(`#${gamePattern[0]}`).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(gamePattern[0]);
+            
+      }, 500);
+    } else {
+      $(`#${gamePattern[0]}`).fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound(gamePattern[0]);
+    }
+    setInterval(function () {
+      $(`#${gamePattern[i]}`).fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound(gamePattern[i]);
+      i++;
+      if (i === gamePattern.length) clearInterval(interval);
+    }, 500);
   }
-  setInterval(function () {
-    $(`#${gamePattern[i]}`).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(gamePattern[i]);
-    i++;
-    if (i === gamePattern.length) clearInterval(interval);
-  }, 500);
-
   //sound
 }
 
@@ -50,8 +87,34 @@ $(".btn").on("click", function () {
 });
 
 function playSound(name) {
-  let sound = new Audio(`./sounds/${name}.mp3`);
-  sound.play();
+  // let sound = new Audio(`./sounds/${name}.mp3`);
+  switch(name) {
+    case "red" : {
+      audio.red.play();
+      break;
+    }
+    case "green": {
+      audio.green.play();
+      break;
+    }
+    case "yellow": {
+      audio.yellow.play();
+      break;
+    }
+    case "blue": {
+      audio.blue.play();
+      break;
+    }
+    case "white": {
+      audio.white.play();
+      break;
+    }
+    case "purple": {
+      audio.purple.play();
+      break;
+    }
+
+  }
 }
 function animation(currentColor) {
   $(`#${currentColor}`).addClass("pressed");
@@ -67,8 +130,7 @@ function checkAnswer() {
   if (count <= gamePattern.length) {
     if (gamePattern[count] !== userClickedPattern[count]) {
       $("h1").html("ПОТРАЧЕНО<br>!!!");
-      let wrong = new Audio("./sounds/wrong.mp3");
-      wrong.play();
+      audio.wrong.play();
       $("body").addClass("game-over");
       // $(".lol").removeClass("hide"); денсая пасхалочка
       setTimeout(function () {
@@ -87,7 +149,7 @@ function checkAnswer() {
       // так как каунт идет с нуля а длинна с 1 нужно вычисть -1 что бы они были равны.
       // переделать в иф елсе стеитмент если уровень делиться на 5 без остатка проигрывать победную музыку и иззменять текст H1
       if (lvl % 10 === 0) {
-        $("h1").html("Excellent");
+        $("h1").html("Excellent<br> stage 2");
         let victory = new Audio("./sounds/victory.mp3");
         victory.play();
         setTimeout(function () {
